d635afd012b86449bd64c7bfb3153a8af1252d95
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-08-15 22:06:52 +0500

    a

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import Image from "next/image";
+import { FcGoogle } from "react-icons/fc";
+import { firebaseAuth } from "../../Utils/firebaseConfig";
+import { useRouter } from 'next/navigation';
 
 const page = () => {
+  const [disable, Setdisable] = useState(false);
+  const router = useRouter();
+  const handleLogin = async () => {
+    Setdisable(true);
+    const provider = new GoogleAuthProvider();
+    const { user } = await signInWithPopup(firebaseAuth, provider);
+    let obj = { name: user.displayName, email: user.email, image: user.photoURL }
+
+    const response = await fetch('http://localhost:4000/user/check', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+    })
+    const data = await response.json();
+    console.log(data);
+    Setdisable(false);
+    if (data.status == false) { Setdisable(true); router.push('/onboarding'); }
+  }
   return (
-    <div>
-      login page
+    <div className="flex items-center justify-center w-full h-screen bg-[url('https://media.giphy.com/media/DRinNvjCXc5Iexx0CH/giphy.gif')] bg-cover bg-center bg-no-repeat">
+      <div className="text-center">
+
+        <button
+          disabled={disable}
+          className="flex items-center px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
+          onClick={handleLogin}
+        >
+          <FcGoogle className="mr-2" />
+          Login with Google
+        </button>
+      </div>
     </div>
   )
 }
